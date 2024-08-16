a8aebd583482078df954af0ea72cfda43cf1563d
Sumit <sumit.tpt07@gmail.com>
2024-08-17 00:49:29 +0530

    update

--- pages/components/Hero.js
+++ pages/components/Hero.js
@@ -7,68 +7,134 @@ export default function Hero({ product }) {
   function addItemToCart() {
     addProduct(product._id);
   }
-  if (product) {
-    return (
-      <div className="relative overflow-hidden bg-background my-14 md:my-10">
-        <div className="lg:py-40 min-h-[650px]">
-          <div className="relative mx-auto sm:static sm:px-6 lg:px-8">
-            <div className="sm:max-w-xl text-start">
-              <h1 className="text-4xl sm:text-5xl lg:text-5xl lg:hidden max-md:mb-6 font-bold tracking-tight text-primary">
-                At <span className="text-accent">50%</span> Off
-              </h1>
-              <h1 className="text-4xl sm:text-5xl lg:text-5xl font-bold tracking-tight text-text">
-                {product.title}
-              </h1>
-              <p className="mt-4 line-clamp-3 text-lg sm:text-xl lg:text-2xl text-gray-500">
-                {product.description}
-              </p>
+  return (
+    // <div className="relative overflow-hidden bg-background my-14 md:my-10">
+    //   <div className="lg:py-40 min-h-[650px]">
+    //     <div className="relative mx-auto sm:static sm:px-6 lg:px-8">
+    //       <div className="sm:max-w-xl text-start">
+    //         <h1 className="text-4xl sm:text-5xl lg:text-5xl lg:hidden max-md:mb-6 font-bold tracking-tight text-primary">
+    //           At <span className="text-accent">50%</span> Off
+    //         </h1>
+    //         <h1 className="text-4xl sm:text-5xl lg:text-5xl font-bold tracking-tight text-text">
+    //           {/* {product.title} */}
+    //         </h1>
+    //         <p className="mt-4 line-clamp-3 text-lg sm:text-xl lg:text-2xl text-gray-500">
+    //           {/* {product.description} */}
+    //         </p>
 
-              <div className="mt-10 flex flex-col max-sm:items-center max-sm:justify-center">
-                {/* Decorative image grid */}
-                <div className="lg:hidden relative ">
-                  <div className="grid grid-cols-1 gap-6">
-                    <div className="w-72 h-80 overflow-hidden rounded-lg border border-secondary transform rotate-3 translate-x-4 hover:-rotate-6 hover:translate-x-8 transition-transform duration-300 ease-in-out">
-                      <img src={product.images[0]} alt="" className="h-full w-full object-cover object-center" />
-                    </div>
-                    <div className="w-72 h-80 overflow-hidden rounded-lg border border-secondary transform -rotate-2 translate-x-2 hover:rotate-4 hover:translate-x-4 transition-transform duration-300 ease-in-out">
-                      <img src={product.images[1]} alt="" className="h-full w-full object-cover object-center" />
-                    </div>
-                  </div>
-                </div>
-                <div className="flex gap-4 items-center max-sm:justify-center max-sm:mt-6">
-                  <button className="mt-6 inline-block rounded-md border border-transparent bg-primary px-6 py-3 text-center font-medium text-white hover:text-accent" onClick={addItemToCart}>
-                    Add to cart
-                  </button>
-                  <Link href="/products" className="mt-6 inline-block rounded-md  bg-transparent border border-accent px-6 py-3 text-center font-medium text-accent hover:text-primary hover:border-primary">
-                    All Products
-                  </Link>
-                </div>
-              </div>
-              <div className="hidden lg:block absolute transform sm:left-1/2 sm:top-0 sm:translate-x-8 lg:left-1/2 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-8">
-                <div className="flex items-center space-x-6 lg:space-x-8">
-                  <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
-                    <div className="w-72 h-80 overflow-hidden rounded-lg border border-secondary transform rotate-3 translate-x-4 hover:-rotate-6 hover:translate-x-8 transition-transform duration-300 ease-in-out">
-                      <img src={product.images[0]} alt="" className="h-full w-full object-cover object-center" />
-                    </div>
-                    <div className="w-72 h-80 overflow-hidden rounded-lg border border-secondary transform -rotate-2 translate-x-2 hover:rotate-4 hover:translate-x-4 transition-transform duration-300 ease-in-out">
-                      <img src={product.images[1]} alt="" className="h-full w-full object-cover object-center" />
-                    </div>
-                  </div>
-                  <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
-                    <div className="w-72 h-80 overflow-hidden rounded-lg border border-secondary transform rotate-1 translate-x-3 hover:-rotate-2 hover:translate-x-4 transition-transform duration-300 ease-in-out">
-                      <img src={product.images[2]} alt="" className="h-full w-full object-cover object-center" />
-                    </div>
-                    <div className="w-72 h-80 overflow-hidden rounded-lg border border-secondary transform -rotate-4 translate-x-2 hover:rotate-8 hover:translate-x-3 transition-transform duration-300 ease-in-out">
-                      <img src={product.images[3]} alt="" className="h-full w-full object-cover object-center" />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+    //         <div className="mt-10 flex flex-col max-sm:items-center max-sm:justify-center">
+    //           {/* Decorative image grid */}
+    //           <div className="lg:hidden relative ">
+    //             <div className="grid grid-cols-1 gap-6">
+    //               <div className="w-72 h-80 overflow-hidden rounded-lg border border-secondary transform rotate-3 translate-x-4 hover:-rotate-6 hover:translate-x-8 transition-transform duration-300 ease-in-out">
+    //                 <img
+    //                   // src={product.images[0]}
+    //                   alt=""
+    //                   className="h-full w-full object-cover object-center"
+    //                 />
+    //               </div>
+    //               <div className="w-72 h-80 overflow-hidden rounded-lg border border-secondary transform -rotate-2 translate-x-2 hover:rotate-4 hover:translate-x-4 transition-transform duration-300 ease-in-out">
+    //                 <img
+    //                   // src={product.images[1]}
+    //                   alt=""
+    //                   className="h-full w-full object-cover object-center"
+    //                 />
+    //               </div>
+    //             </div>
+    //           </div>
+    //           <div className="flex gap-4 items-center max-sm:justify-center max-sm:mt-6">
+    //             <button
+    //               className="mt-6 inline-block rounded-md border border-transparent bg-primary px-6 py-3 text-center font-medium text-white hover:text-accent"
+    //               onClick={addItemToCart}
+    //             >
+    //               Add to cart
+    //             </button>
+    //             <Link
+    //               href="/products"
+    //               className="mt-6 inline-block rounded-md  bg-transparent border border-accent px-6 py-3 text-center font-medium text-accent hover:text-primary hover:border-primary"
+    //             >
+    //               All Products
+    //             </Link>
+    //           </div>
+    //         </div>
+    //         <div className="hidden lg:block absolute transform sm:left-1/2 sm:top-0 sm:translate-x-8 lg:left-1/2 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-8">
+    //           <div className="flex items-center space-x-6 lg:space-x-8">
+    //             <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
+    //               <div className="w-72 h-80 overflow-hidden rounded-lg border border-secondary transform rotate-3 translate-x-4 hover:-rotate-6 hover:translate-x-8 transition-transform duration-300 ease-in-out">
+    //                 <img
+    //                   // src={product.images[0]}
+    //                   alt=""
+    //                   className="h-full w-full object-cover object-center"
+    //                 />
+    //               </div>
+    //               <div className="w-72 h-80 overflow-hidden rounded-lg border border-secondary transform -rotate-2 translate-x-2 hover:rotate-4 hover:translate-x-4 transition-transform duration-300 ease-in-out">
+    //                 <img
+    //                   // src={product.images[1]}
+    //                   alt=""
+    //                   className="h-full w-full object-cover object-center"
+    //                 />
+    //               </div>
+    //             </div>
+    //             <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
+    //               <div className="w-72 h-80 overflow-hidden rounded-lg border border-secondary transform rotate-1 translate-x-3 hover:-rotate-2 hover:translate-x-4 transition-transform duration-300 ease-in-out">
+    //                 <img
+    //                   // src={product.images[2]}
+    //                   alt=""
+    //                   className="h-full w-full object-cover object-center"
+    //                 />
+    //               </div>
+    //               <div className="w-72 h-80 overflow-hidden rounded-lg border border-secondary transform -rotate-4 translate-x-2 hover:rotate-8 hover:translate-x-3 transition-transform duration-300 ease-in-out">
+    //                 <img
+    //                   // src={product.images[3]}
+    //                   alt=""
+    //                   className="h-full w-full object-cover object-center"
+    //                 />
+    //               </div>
+    //             </div>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+
+    <div class="bg-white dark:bg-gray-800 flex relative z-20 items-center overflow-hidden">
+      <div class="container mx-auto px-6 flex relative py-16">
+        <div class="sm:w-2/3 lg:w-2/5 flex flex-col relative z-20">
+          <span class="w-20 h-2 bg-gray-800 dark:bg-white mb-12"></span>
+          <h1 class="font-bebas-neue uppercase text-6xl sm:text-8xl font-black flex flex-col leading-none dark:text-white text-gray-800">
+            Wear the
+            <span class="text-5xl sm:text-7xl">Best</span>
+          </h1>
+          <p class="text-sm sm:text-base text-gray-700 dark:text-white">
+            We deliver the best Glasses of all types. Lorem Ipsum is simply
+            dummy text of the printing and typesetting industry. Lorem Ipsum is
+            simply dummy text of the printing and typesetting industry.
+          </p>
+          <div class="flex mt-8">
+            <a
+              href="#"
+              class="uppercase py-2 px-4 rounded-lg bg-pink-500 border-2 border-transparent text-white text-md mr-4 hover:bg-pink-400"
+            >
+              Get started
+            </a>
+            <a
+              href="#"
+              class="uppercase py-2 px-4 rounded-lg bg-transparent border-2 border-pink-500 text-pink-500 dark:text-white hover:bg-pink-500 hover:text-white text-md"
+            >
+              Read more
+            </a>
           </div>
         </div>
+        <div class="hidden sm:block sm:w-1/3 lg:w-3/5 relative">
+          <img
+            src="https://png.pngtree.com/background/20230516/original/pngtree-an-older-asian-man-with-sunglasses-outside-of-the-city-picture-image_2610389.jpg"
+            class="max-w-xs md:max-w-sm m-auto"
+          />
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
+
   return null;
 }
--- pages/components/Products.js
+++ pages/components/Products.js
@@ -9,67 +9,179 @@ const formatPrice = (price) => {
 };
 
 export default function Products({ products }) {
-  const { addProduct } = useContext(CartContext)
+  const { addProduct } = useContext(CartContext);
 
   return (
     <div className="bg-white">
       <div className="mx-auto px-4 py-6">
-        <h2 className="text-2xl font-bold tracking-tight text-text">Our Latest Products</h2>
+        <h2 className="text-2xl font-bold tracking-tight text-text">
+          Our Latest Products
+        </h2>
 
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-5 xl:gap-x-8">
-          {products?.length > 0 && products.map((product) => (
-            <div key={product.id} className="group relative">
-              <div className="group block overflow-hidden border border-accent rounded-xl border-opacity-10">
-                <div className="p-1">
-                  <div className="relative h-[300px] sm:h-[300px]">
-                    <img
-                      src={product.images[0]}
-                      alt=""
-                      className="absolute inset-0 h-full w-full object-contain opacity-100 group-hover:opacity-0"
-                    />
+          {products?.length > 0 &&
+            products.map((product) => (
+              // <div key={product.id} className="group relative">
+              //   <div className="group block overflow-hidden border border-accent rounded-xl border-opacity-10">
+              //     <div className="p-1">
+              //       <div className="relative h-[300px] sm:h-[300px]">
+              //         <img
+              //           src={product.images[0]}
+              //           alt=""
+              //           className="absolute inset-0 h-full w-full object-contain opacity-100 group-hover:opacity-0"
+              //         />
 
-                    <img
-                      src={product.images[1]}
-                      alt=""
-                      className="absolute inset-0 h-full w-full object-contain opacity-0 group-hover:opacity-100"
-                    />
-                  </div>
+              //         <img
+              //           src={product.images[1]}
+              //           alt=""
+              //           className="absolute inset-0 h-full w-full object-contain opacity-0 group-hover:opacity-100"
+              //         />
+              //       </div>
 
-                  <div className="relative  p-3 border-t">
-                    <Link href={'/products/' + product._id}>
-                      <h3
-                        className="text-md text-gray-700 group-hover:underline group-hover:underline-offset-4 truncate"
+              //       <div className="relative  p-3 border-t">
+              //         <Link href={'/products/' + product._id}>
+              //           <h3
+              //             className="text-md text-gray-700 group-hover:underline group-hover:underline-offset-4 truncate"
+              //           >
+              //             {product.title}
+              //           </h3>
+              //         </Link>
+
+              //         <div className="mt-1.5 flex items-center justify-between text-text">
+              //           <p className="tracking-wide text-primary">ksh. {formatPrice(product.price)}</p>
+
+              //           <button onClick={() => {addProduct(product._id);
+              //              toast.success('Item added to cart!!')}} type="button" class="flex items-center divide-x rounded-lg border border-primary bg-white text-center text-md font-medium text-secondary-700 shadow-sm hover:bg-gray-100">
+              //             <div class="flex items-center space-x-2 py-2.5 px-3">
+              //               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+              //                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+              //               </svg>
+
+              //               <span>Add</span>
+              //             </div>
+              //             {/* <div class="py-2.5 px-3">18</div> */}
+              //           </button>
+
+              //         </div>
+              //       </div>
+              //     </div>
+              //   </div>
+
+              // </div>
+              <div
+                key={product.id}
+                class="relative m-10 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md"
+              >
+                <Link
+                  href={"/products/" + product._id}
+                  class="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl"
+                >
+                  <img
+                    class="object-cover"
+                    src={product.images[0]}
+                    alt="product image"
+                  />
+                  <span class="absolute top-0 left-0 m-2 rounded-full bg-black px-2 text-center text-sm font-medium text-white">
+                    39% OFF
+                  </span>
+                </Link>
+                <div class="mt-4 px-5 pb-5">
+                  <a href="#">
+                    <h5 class="text-xl tracking-tight text-slate-900">
+                      {product.title}
+                    </h5>
+                  </a>
+                  <div class="mt-2 mb-5 flex items-center justify-between">
+                    <p>
+                      <span class="text-3xl font-bold text-slate-900">
+                        ${product.price}
+                      </span>
+                      <span class="text-sm text-slate-900 line-through">
+                        $
+                        {(product.price + (product.price * 39) / 100).toFixed(
+                          2
+                        )}
+                      </span>
+                    </p>
+                    <div class="flex items-center">
+                      <svg
+                        aria-hidden="true"
+                        class="h-5 w-5 text-yellow-300"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
                       >
-                        {product.title}
-                      </h3>
-                    </Link>
-
-                    <div className="mt-1.5 flex items-center justify-between text-text">
-                      <p className="tracking-wide text-primary">ksh. {formatPrice(product.price)}</p>
-
-
-                      <button onClick={() => {addProduct(product._id);
-                         toast.success('Item added to cart!!')}} type="button" class="flex items-center divide-x rounded-lg border border-primary bg-white text-center text-md font-medium text-secondary-700 shadow-sm hover:bg-gray-100">
-                        <div class="flex items-center space-x-2 py-2.5 px-3">
-                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-                          </svg>
-
-
-                          <span>Add</span>
-                        </div>
-                        {/* <div class="py-2.5 px-3">18</div> */}
-                      </button>
-
+                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                      </svg>
+                      <svg
+                        aria-hidden="true"
+                        class="h-5 w-5 text-yellow-300"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                      </svg>
+                      <svg
+                        aria-hidden="true"
+                        class="h-5 w-5 text-yellow-300"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                      </svg>
+                      <svg
+                        aria-hidden="true"
+                        class="h-5 w-5 text-yellow-300"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                      </svg>
+                      <svg
+                        aria-hidden="true"
+                        class="h-5 w-5 text-yellow-300"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                      </svg>
+                      <span class="mr-2 ml-3 rounded bg-yellow-200 px-2.5 py-0.5 text-xs font-semibold">
+                        5.0
+                      </span>
                     </div>
                   </div>
+                  <a
+                    onClick={() => {
+                      addProduct(product._id);
+                      toast.success("Item added to cart!!");
+                    }}
+                    class="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="mr-2 h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                      />
+                    </svg>
+                    Add to cart
+                  </a>
                 </div>
               </div>
-
-            </div>
-          ))}
+            ))}
         </div>
       </div>
     </div>
-  )
+  );
 }
