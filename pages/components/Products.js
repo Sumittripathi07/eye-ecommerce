049f2529a5b5dbff5b6ae8df1863b1a780e47558
Sumit <sumit.tpt07@gmail.com>
2024-08-25 18:41:27 +0530

    update

--- pages/components/Products.js
+++ pages/components/Products.js
@@ -13,12 +13,12 @@ export default function Products({ products }) {
 
   return (
     <div className="bg-white">
-      <div className="mx-auto px-4 py-6">
+      <div className="px-4 py-6 mx-auto">
         <h2 className="text-2xl font-bold tracking-tight text-text">
           Our Latest Products
         </h2>
 
-        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-5 xl:gap-x-8">
+        <div className="grid grid-cols-1 mt-6 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-5 xl:gap-x-8">
           {products?.length > 0 &&
             products.map((product) => (
               <div
