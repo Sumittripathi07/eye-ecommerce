b6cefc59ffd4e3b702b2f12372c360613f031f19
Sumit <sumit.tpt07@gmail.com>
2024-09-04 12:23:33 +0530

    service card

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,34 @@ import Hero from "./components/Hero";
 import { Product } from "@/models/Product";
 import Products from "./components/Products";
 import Collection from "./components/Collection";
+import ServiceCard from "./components/ServiceCard";
+import {
+  facebook,
+  instagram,
+  shieldTick,
+  support,
+  truckFast,
+  twitter,
+} from "../public/assets/icons";
+
+const services = [
+  {
+    imgURL: truckFast,
+    label: "Free shipping",
+    subtext: "Enjoy seamless shopping with our complimentary shipping service.",
+  },
+  {
+    imgURL: shieldTick,
+    label: "Secure Payment",
+    subtext:
+      "Experience worry-free transactions with our secure payment options.",
+  },
+  {
+    imgURL: support,
+    label: "Love to help you",
+    subtext: "Our dedicated team is here to assist you every step of the way.",
+  },
+];
 
 export default function Home({
   featuredProduct,
@@ -19,6 +47,11 @@ export default function Home({
       <Products products={newProducts} />
       <hr className="h-px my-1 bg-gray-300 border-0" />
       <Collection product={collectionProduct1} />
+      <section className="flex flex-wrap justify-center max-container gap-9">
+        {services.map((service) => (
+          <ServiceCard key={service.label} {...service} />
+        ))}
+      </section>
     </main>
   );
 }
